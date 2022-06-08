cc93982806bfe1ade62f22d803e3e2c630d2d1a9
JEY <jey.pley@gmail.com>
2022-06-08 18:24:14 +0200

    mongodb configuration

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 import express from "express"; // Need to add {"type": "module"} in package.json
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+// Initialize the .env file
+dotenv.config();
+
+// Initialize the express app
 const app = express();
 
-app.listen(8080, () => console.log(`Server running on port 8080.`));
+// Start the DB connection
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log("Successfully connected to MongoDB ✅");
+  } catch (error) {
+    throw error;
+  }
+};
+
+// In case of deconnection with the DB
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB connection is now closed ⚠️");
+});
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connection is now open 📖");
+});
+
+// Start the backend server
+const PORT = 8080 || process.env.PORT;
+app.listen(PORT, () => {
+  connect();
+  console.log(`Server running on port ${PORT} 🏃‍♀️`);
+});
