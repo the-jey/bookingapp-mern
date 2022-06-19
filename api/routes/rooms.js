9e2fbd3c73ceccbe92b35e494f48effdf0f6918d
JEY <jey.pley@gmail.com>
2022-06-19 18:06:10 +0200

    rooms crud and model

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,9 +1,20 @@
 import express from "express";
 
+import {
+  createRoom,
+  deleteRoom,
+  getAllRooms,
+  getRoom,
+  updateRoom,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verify.js";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello, this the rooms endpoint.");
-});
+router.post("/:hotelid", verifyAdmin, createRoom);
+router.put("/:id", verifyAdmin, updateRoom);
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+router.get("/:id", getRoom);
+router.get("/", getAllRooms);
 
 export default router;
